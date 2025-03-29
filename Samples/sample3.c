809c6377c8d80619c490354c8e77b54ccf6fcfa3
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-29 23:25:56 +0900

    add result

--- Samples/sample3.c
+++ Samples/sample3.c
@@ -49,3 +49,34 @@ int main(int argc, char* argv[])
         MPI_Finalize();
         return 0;
 }
+
+// salloc -p ${PART} -N 1\
+//             -w ${NODE}\
+//             -- mpirun -np ${NP} ./sample3
+
+
+// -bash-4.2$ mpicc -o sample3 sample3.c
+// -bash-4.2$ PART=comq
+// -bash-4.2$ NODE=ppx00
+// -bash-4.2$ salloc -p ${PART} -N 1\
+// >             -w ${NODE}\
+// >             -- mpirun -np ${NP} ./sample3
+// salloc: Granted job allocation 305197
+// [ppx00][[546,1],2][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// [ppx00][[546,1],6][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// [ppx00][[546,1],3][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// [ppx00][[546,1],5][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// [ppx00][[546,1],1][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// [ppx00][[546,1],0][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// --------------------------------------------------------------------------
+// WARNING: There was an error initializing an OpenFabrics device.
+
+//   Local host:   ppx00
+//   Local device: mlx5_0
+// --------------------------------------------------------------------------
+// [ppx00][[546,1],4][btl_openib_component.c:1670:init_one_device] error obtaining device attributes for mlx5_0 errno says No space left on device
+// [0/7] send data = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,}
+// [1/7] recv data = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,}
+// [ppxsvc.ccs.tsukuba.ac.jp:52601] 6 more processes have sent help message help-mpi-btl-openib.txt / error in device init
+// [ppxsvc.ccs.tsukuba.ac.jp:52601] Set MCA parameter "orte_base_help_aggregate" to 0 to see all help / error messages
+// salloc: Relinquishing job allocation 305197
