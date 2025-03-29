30c6ceb7d3e3a8c93b738231e383074d1c96e0db
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-30 00:38:52 +0900

    Chage

--- Samples/openMP/mpisum.c
+++ Samples/openMP/mpisum.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int A[1000];
-int sum(int *a, int n)
-{
-int s, i;
-s = 0;
-for (i=0; i<n; i++) s += a[i];
-return s;
+
+int sum(int *a, int n) {
+    int s = 0, i;
+    for (i = 0; i < n; i++)
+        s += a[i];
+    return s;
 }
 
-int main()
-{
-int i;
-for (i=0; i<1000; i++) A[i] = i;
-printf(sum = %d\n'', sum(A,1000));
+int main() {
+    int i;
+    for (i = 0; i < 1000; i++)
+        A[i] = i;
+
+    printf("sum = %d\n", sum(A, 1000));
+
+    return 0;
 }
