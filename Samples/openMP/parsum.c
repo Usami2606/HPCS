01a9b269d2cec898414b9693ea7ea9248696dcb5
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-31 07:49:56 +0900

    add new file

---
+++ Samples/openMP/parsum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int A[1000];
+
+int sum(int *a, int n)
+{
+int s, i;
+s = 0;
+for (i=0; i<n; i++) s += a[i];
+return s;
+int main()
+}
+{
+int i;
+for (i=0; i<1000; i++) A[i] = i;
+printf(sum = %d\n'', sum(A,1000));
+}
