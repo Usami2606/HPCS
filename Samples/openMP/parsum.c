61165d9b0b1d5cab71e4df365b65cf20f85cc5dd
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-31 09:59:52 +0900

    /

--- Samples/openMP/parsum.c
+++ Samples/openMP/parsum.c
@@ -3,14 +3,28 @@
 int A[1000];
 
 int sum(int *a, int n) {
-int s, i;
-s = 0;
-for (i=0; i<n; i++) s += a[i];
-return s;
+    int s, i;
+    s = 0;
+#pragma omp parallel
+{
+#pragma omp reduction(+:s)
+    for (i = 0; i < n; i++)
+        s += a[i];
+}
+    return s;
 }
 
-int main(){
-int i;
-for (i=0; i<1000; i++) A[i] = i;
-printf("sum = %d\n", sum(A,1000));
+int main() {
+    int i;
+    for (i = 0; i < 1000; i++)
+        A[i] = i;
+
+    printf("sum = %d\n", sum(A, 100));
+
+    return 0;
 }
+
+// -bash-4.2$ gcc -fopenmp -o mpisum mpisum.c
+// -bash-4.2$ ./mpisum
+// sum = 499500
+// -bash-4.2$ 
