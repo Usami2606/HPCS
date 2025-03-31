124e46fb0756a416a681c33aba372596ab4fd02e
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-31 10:45:29 +0900

    add new folder and file

---
+++ assign2/sum1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int A[1000];
+
+int sum(int *a, int n) {
+    int s, i;
+    s = 0;
+#pragma omp parallel
+{
+#pragma omp for reduction(+:s)
+    printf("Hello World from %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+    for (i = 0; i < n; i++)
+        s += a[i];
+}
+    return s;
+}
+
+int main() {
+    int i;
+    for (i = 0; i < 1000; i++)
+        A[i] = i;
+
+    printf("sum = %d\n", sum(A, 1000));
+
+    return 0;
+}
+
