51c24c965a9662fa3c7d0dd8d25b79a878289ecd
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-31 11:01:28 +0900

    add new file

--- assign2/sum2.c
+++ assign2/sum2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <omp.h>
 
-int A[1000];
+float A[1000];
 
-int sum(int *a, int n) {
-    int s, i;
-    s = 0;
+float sum(float *a, int n) {
+    int i;
+    float s = 0.0f;
 #pragma omp parallel
 {
-    // printf("Hello World from %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
+    printf("Hello World from %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
 #pragma omp for reduction(+:s)
     
     for (i = 0; i < n; i++)
@@ -20,7 +20,7 @@ int sum(int *a, int n) {
 int main() {
     int i;
     for (i = 0; i < 1000; i++)
-        A[i] = i;
+        A[i] = (float)i + 0.5f;
 
     printf("sum = %d\n", sum(A, 1000));
 
