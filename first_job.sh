27e72b1d31d542bd411ffd6973e2971be663b18f
Usami2606 <usami2606tsukuba@gmail.com>
2025-03-31 11:48:49 +0900

    add new file

--- first_job.sh
+++ first_job.sh
@@ -1,4 +1,5 @@
 # load MPI enviroment
+module avail
 module load gcc/4.8.5
 module load openmpi/3.1.0
 
---
+++ job.sh
@@ -0,0 +1,4 @@
+# load MPI enviroment
+module avail
+module load gcc/4.8.5
+module load openmpi/3.1.0
