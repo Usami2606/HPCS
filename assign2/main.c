99db558777ed8f522beeb149397f8ce8c36b67db
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-01 16:00:39 +0900

    change

--- assign2/main.c
+++ assign2/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <omp.h>
 
-int A[1000000];
+int A[100000000];
 
 int main() {
     double starttime = second();
 
     int i;
-    for (i = 0; i < 1000000; i++)
+    for (i = 0; i < 100000000; i++)
         A[i] = i;
 
-    printf("sum = %d\n", sum(A, 1000000));
+    printf("sum = %d\n", sum(A, 100000000));
 
     double endtime = second();
     printf("%f\n", endtime - starttime);
