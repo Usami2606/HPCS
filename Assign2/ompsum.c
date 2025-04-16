8f8faa7152a571c34a6c36fd6751d79f6cfb4f77
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-16 20:10:03 +0900

    add thread

--- Assign2/ompsum.c
+++ Assign2/ompsum.c
@@ -23,6 +23,7 @@ int main() {
     int i;
     for (i = 0; i < 100000000; i++) A[i] = i;
     // printf("sum = %lld\n", sum(A, 100000000));
+    printf("Thread is  %d\n",omp_get_num_threads());
     end = second();
     printf("time = %f seconds\n", end - start);
 }
