475e08c1d15a5e014fb1d194e2e3ebe697bb2373
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-16 19:34:13 +0900

    add new function to run.sh

--- Assign2/run.sh
+++ Assign2/run.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# 引数でスレッド数を指定し、デフォルトは 4
-OMP_NUM_THREADS=${1:-4}
+# Initial number of threads
+threads=4
 
-# OMP_NUM_THREADS を設定
-export OMP_NUM_THREADS
+# 最大スレッド数（1024）
+max_threads=1024
 
-# 実行
-echo "Using $OMP_NUM_THREADS threads"
-./bin/ompsum "$@"
+# スレッド数が最大を超えるまで繰り返す
+while [ $threads -le $max_threads ]; do
+    # OMP_NUM_THREADS を設定
+    export OMP_NUM_THREADS=$threads
+
+    # 実行
+    echo "Running with $OMP_NUM_THREADS threads"
+    ./bin/ompsum
+
+    # 次のスレッド数を倍にする
+    threads=$((threads * 2))
+done
