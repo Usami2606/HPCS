1aece4e30a2b987d0c3f8f3d7b3877c1879376ab
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-16 19:15:32 +0900

    add ompsum.c and Makefile

---
+++ Assign2/ompsum.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+double second();
+
+int A[100000000];
+
+long long sum(int *a, int n) {
+    int i;
+    long long s = 0;
+#pragma omp parallel
+{
+#pragma omp for reduction(+:s)
+    for (i = 0; i < n; i++) s += a[i];
+}
+    return s;
+}
+
+int main() {
+    double start, end;
+
+    start = second();
+    int i;
+    for (i = 0; i < 100000000; i++) A[i] = i;
+    printf("sum = %lld\n", sum(A, 100000000));
+    end = second();
+    printf("time = %f seconds\n", end - start);
+}
+
+double second()
+{
+    struct timeval tm;
+    double t ;
+
+    gettimeofday(&tm,NULL);
+    t = (double) (tm.tv_sec) + ((double) (tm.tv_usec))/1.0e6;
+    return t;
+}
