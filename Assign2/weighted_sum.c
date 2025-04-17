bba9d6486851bc4ba180de7c25d35838822f4110
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-17 14:56:50 +0900

    add weighted_sum.c

---
+++ Assign2/weighted_sum.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
+#include <omp.h>
+
+double second();
+
+#define N 100000000
+
+double A[N];  // 配列を double に
+
+double sum(double *a, int n) {
+    int i;
+    double s = 0.0;
+#pragma omp parallel for reduction(+:s)
+    for (i = 0; i < n; i++) s += a[i];
+    return s;
+}
+
+int main() {
+    double start, end;
+    int i;
+
+    start = second();
+
+#pragma omp parallel for
+    for (i = 0; i < N; i++) {
+        double m = (double)(i + 1);  // m = 1〜N
+        A[i] = (1.0 / m) * sin(1.0 / m);
+    }
+
+    printf("sum = %.12f\n", sum(A, N));
+
+    end = second();
+    printf("time = %f seconds\n", end - start);
+
+    return 0;
+}
+
+double second() {
+    struct timeval tm;
+    gettimeofday(&tm, NULL);
+    return (double)(tm.tv_sec) + (double)(tm.tv_usec) / 1.0e6;
+}
