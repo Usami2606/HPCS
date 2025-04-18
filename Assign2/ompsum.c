bc0fa56a423513a284a0aa702bb609179022d92d
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-18 16:13:56 +0900

    20times

--- Assign2/ompsum.c
+++ Assign2/ompsum.c
@@ -19,21 +19,44 @@ long long sum(int *a, int n) {
 
 int main() {
     double start, end;
+    double times[20];  // 計測時間を格納する配列
+    int i, j;
 
-    start = second();
-    int i;
-    for (i = 0; i < 100000000; i++) A[i] = i;
-    printf("sum = %lld\n", sum(A, 100000000));
-    end = second();
-    printf("time = %f seconds\n", end - start);
+    // 計測を20回行う
+    for (j = 0; j < 20; j++) {
+        start = second();
+        for (i = 0; i < 100000000; i++) A[i] = i;
+        sum(A, 100000000);  // 計算の実行
+        end = second();
+        times[j] = end - start;  // 計測時間を配列に保存
+    }
+
+    // 平均、最大値、最小値を計算
+    double total_time = 0.0;
+    double max_time = times[0];
+    double min_time = times[0];
+
+    for (i = 0; i < 20; i++) {
+        total_time += times[i];
+        if (times[i] > max_time) max_time = times[i];
+        if (times[i] < min_time) min_time = times[i];
+    }
+
+    double average_time = total_time / 20.0;
+
+    // 結果を出力
+    printf("Average time = %f seconds\n", average_time);
+    printf("Maximum time = %f seconds\n", max_time);
+    printf("Minimum time = %f seconds\n", min_time);
+
+    return 0;
 }
 
-double second()
-{
+double second() {
     struct timeval tm;
-    double t ;
+    double t;
 
-    gettimeofday(&tm,NULL);
-    t = (double) (tm.tv_sec) + ((double) (tm.tv_usec))/1.0e6;
+    gettimeofday(&tm, NULL);
+    t = (double)(tm.tv_sec) + ((double)(tm.tv_usec)) / 1.0e6;
     return t;
 }
--- Assign2/run.sh
+++ Assign2/run.sh
@@ -1,36 +1,36 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Initial number of threads
-# threads=1
+# Initial number of threads
+threads=1
 
-# # 最大スレッド数（1024）
-# max_threads=512
+# 最大スレッド数（1024）
+max_threads=512
 
-# # スレッド数が最大を超えるまで繰り返す
-# while [ $threads -le $max_threads ]; do
-#     # OMP_NUM_THREADS を設定
-#     export OMP_NUM_THREADS=$threads
+# スレッド数が最大を超えるまで繰り返す
+while [ $threads -le $max_threads ]; do
+    # OMP_NUM_THREADS を設定
+    export OMP_NUM_THREADS=$threads
 
-#     # 実行
-#     echo "Running with $OMP_NUM_THREADS threads"
-#     ./bin/runprog
+    # 実行
+    echo "Running with $OMP_NUM_THREADS threads"
+    ./bin/runprog
 
-#     # 次のスレッド数を倍にする
-#     threads=$((threads * 2))
-# done
+    # 次のスレッド数を倍にする
+    threads=$((threads * 2))
+done
 
 #!/bin/bash
 
-# 引数チェック
-if [ -z "$1" ]; then
-    echo "Usage: $0 <num_threads>"
-    exit 1
-fi
+# # 引数チェック
+# if [ -z "$1" ]; then
+#     echo "Usage: $0 <num_threads>"
+#     exit 1
+# fi
 
-# 引数でスレッド数を設定
-export OMP_NUM_THREADS=$1
+# # 引数でスレッド数を設定
+# export OMP_NUM_THREADS=$1
 
-# 実行
-echo "Running with $OMP_NUM_THREADS threads"
-./bin/runprog
+# # 実行
+# echo "Running with $OMP_NUM_THREADS threads"
+# ./bin/runprog
 
