353b722138ac0f256f1c25b0e43ccf5e2c9b6a12
Usami2606 <usami2606tsukuba@gmail.com>
2025-04-23 17:02:01 +0900

    2

--- Sample/Sample1.c
+++ Sample/Sample1.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
-#include <unistd.h> // gethostname()
 
 int main(int argc, char* argv[])
 {
-        int myrank, nprocs;
-        char hostname[64]; 
+
+        int myrank, nprocs, i;
+        int *buf;
+        MPI_Status status;
 
         MPI_Init(&argc, &argv);
 
-        //ホスト名を取得
-        gethostname(hostname, 64);
-        //rank番号の取得
         MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-        //起動プロセス数の取得
         MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-        //
-        printf("[%d/%d] my host name is %s!!!\n", myrank, nprocs, hostname);
 
+        buf = malloc(sizeof(int)*20);
+
+        if(myrank == 0){
+                for(i=0;i<20;i++){
+                        buf[i] = i;
+                }
+        }
+
+        if(myrank == 0){
+                MPI_Send(buf, 20, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        }else if(myrank == 1){
+                MPI_Recv(buf, 20, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        }
+
+        if(myrank == 0){
+                printf("[%d/%d] send data = {", myrank, nprocs);
+                for(i=0;i<20;i++){
+                        printf("%d,", buf[i]);
+                }
+                printf("}\n");
+        }
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        if(myrank == 1){
+                printf("[%d/%d] recv data = {", myrank, nprocs);
+                for(i=0;i<20;i++){
+                        printf("%d,", buf[i]);
+                }
+                printf("}\n");
+        }
 
         MPI_Finalize();
         return 0;
