e861e835d347c8b0e0a04063a1c74e39fb7bbf2d
Usami2606 <usami2606tsukuba@gmail.com>
2025-05-01 18:47:40 +0900

    bug fix

--- Assign3/run.sh
+++ Assign3/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-    echo "Usage: $0 <num_threads> <num_procs>"
+    echo "Usage: $0 <num_threads> <num_procs> <num_node>"
     exit 1
 fi
 
 # 引数でスレッド数とプロセス数を設定
 export OMP_NUM_THREADS=$1
 NUM_PROCS=$2
+NUM_NODE=$3
 
 MV2_ENABLE_AFFINITY=0
 # Execution
@@ -15,10 +16,6 @@ MV2_ENABLE_AFFINITY=0
 PART=comq
 NODE=ppx00
 echo "Running with $OMP_NUM_THREADS threads and $NUM_PROCS MPI processes"
-salloc -p ${PART} -N 1\
-            -w ${NODE}\
-            mpirun -np $NUM_PROCS --mca mpi_warn_on_fork 0 ./bin/runprog
-
-salloc -p ${PART} -N 2\
+salloc -p ${PART} -N ${NUM_NODE}\
             -w ${NODE}\
             mpirun -np $NUM_PROCS --mca mpi_warn_on_fork 0 ./bin/runprog
