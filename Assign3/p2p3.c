1d005cbe36993d180579e10a288ef0a0d34780e7
Usami2606 <usami2606tsukuba@gmail.com>
2025-05-02 17:01:56 +0900

    add new file

---
+++ Assign3/p2p3.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+double second() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec / 1000000.0;
+}
+
+int main(int argc, char* argv[])
+{
+    double start, end;
+    int m;
+    double times[20];
+    int myrank, nprocs, i;
+    double *sendbuf, *recvbuf;  // 配列の型をdoubleに変更
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    printf("my rank is %d and size is %d\n",myrank, nprocs);
+
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    printf("Rank %d running on %s\n", myrank, hostname);
+    
+
+    // ここでプロセス数が1でないことを確認
+    if (nprocs < 2) {
+        if (myrank == 0) {
+            fprintf(stderr, "This program requires at least 2 processes.\n");
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
+
+    int size = m * 1000000;  // 配列のサイズを変更
+    sendbuf = malloc(sizeof(double) * size);  // 動的にサイズを変更
+    recvbuf = malloc(sizeof(double) * size);
+    // mallocの成功をチェック
+    if (sendbuf == NULL) {
+        fprintf(stderr, "Memory allocation failed for size %d\n", size);
+        MPI_Abort(MPI_COMM_WORLD, 1);  // メモリ割り当てに失敗した場合、MPIプログラムを終了
+    }
+
+    if (myrank == 0) {
+        // 送信するデータを設定
+        for (i = 0; i < size; i++) {
+            sendbuf[i] = (double)i * 1.1;  // 例として、iを1.1倍した値を設定
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);  // 同期を取る
+    start = second();
+    // データを送信・受信
+    if (myrank == 0) {
+        MPI_Send(sendbuf, size, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+    } else if (myrank == 1) {
+        MPI_Recv(recvbuf, size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);  
+    end = second();
+
+    if (myrank == 0) {
+        times[0] = end - start
+        printf("%f\n", times[0]);
+    } else if (myrank == 1) {
+        times[1] = end - start
+        printf("%f\n", times[1]);
+    }
+
+
+    // bufのメモリ解放
+    free(sendbuf);
+    free(recvbuf);
+    
+
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+    return 0;
+}
