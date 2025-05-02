d6c7554458719e19568ac17bde9bcd6b10f397cf
Usami2606 <usami2606tsukuba@gmail.com>
2025-05-02 15:14:22 +0900

    bug fix

--- Assign3/job.sh
+++ Assign3/job.sh
@@ -17,5 +17,5 @@ PART=bdw-mixed
 NODE=ppx00
 echo "Running with $OMP_NUM_THREADS threads and $NUM_PROCS MPI processes and $NODE_NUMBER Node"
 salloc -p ${PART}  -N ${NUM_NODE} -n ${NUM_PROCS} --ntasks-per-core=1\
-            -w ppx00,ppx02\
+            -w ppx00\
             mpirun -np $NUM_PROCS --mca mpi_warn_on_fork 0 ./bin/runprog
