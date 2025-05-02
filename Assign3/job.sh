603e2996eb8e8d17bc323f70a24bb5a3243b63f8
Usami2606 <usami2606tsukuba@gmail.com>
2025-05-02 15:15:50 +0900

    bug fix

--- Assign3/job.sh
+++ Assign3/job.sh
@@ -16,6 +16,6 @@ MV2_ENABLE_AFFINITY=1
 PART=bdw-mixed
 NODE=ppx00
 echo "Running with $OMP_NUM_THREADS threads and $NUM_PROCS MPI processes and $NODE_NUMBER Node"
-salloc -p ${PART}  -N ${NUM_NODE} -n ${NUM_PROCS} --ntasks-per-core=1\
+salloc -p ${PART}  -N ${NUM_NODE} -n ${NUM_PROCS} --ntasks-per-core=1 \
             -w ppx00\
             mpirun -np $NUM_PROCS --mca mpi_warn_on_fork 0 ./bin/runprog
